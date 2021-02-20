da591f44c6edf77548f218b36836813c1373e42a
test <test@gmail.com>
2021-02-19 23:24:49 -0500

    lab 2

---
+++ index.php
@@ -0,0 +1,54 @@
+<main>
+    <link rel="styleshet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>fjhskldjfkdsjflisjfP:ASkd</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2 style="color: pink "> CEO</h2>
+                    <p>Assel Alabbadi</p>
+                    <p> Email: alabbadi@cs230.com</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.peng">
+                <div class="container">
+                    <h2>Human resource</h2>
+                    <p>Lama farhan</p>
+                    <p> Email: farhan@cs230.com</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>Accountant</h2>
+                    <p>Hashim Alalwi</p>
+                    <p> Email: alalwi@cs230.com</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.peng">
+                <div class="container">
+                    <h2>Marketing</h2>
+                    <p>Sawson faisal</p>
+                    <p> Email: faisal@cs230.com</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
